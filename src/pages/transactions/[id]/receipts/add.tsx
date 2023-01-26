2f3a28af6cc639d2eb9aba5b6a9a139fc8a68b86
Angelo Geulin <angelogeulin123@gmail.com>
2023-01-26 23:58:37 +0800

    style: change placeholder text

--- src/pages/transactions/[id]/receipts/add.tsx
+++ src/pages/transactions/[id]/receipts/add.tsx
@@ -97,7 +97,7 @@ export default function AddReceipt() {
           <input
             type="text"
             className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-300/40"
-            placeholder="Enter a meaningful title ..."
+            placeholder="A meaningful title ..."
             {...register("title")}
           />
           {errors.title && (
