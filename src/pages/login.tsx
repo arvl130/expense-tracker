1ab9b13ebe46662a7a51287d98a88d882701ada2
Angelo Geulin <angelogeulin123@gmail.com>
2023-02-15 20:00:50 +0800

    style: add message to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,28 @@
 import Loading from "@/components/Loading"
+import { signIn } from "next-auth/react"
 import { useRedirectOnAuthenticated } from "@/hooks/useRedirect"
-import { api } from "@/utils/api"
 
 export default function Home() {
   const { status } = useRedirectOnAuthenticated()
   if (status !== "unauthenticated") return <Loading />
 
-  return <main className="max-w-6xl mx-auto">This is the login page.</main>
+  return (
+    <main className="max-w-6xl mx-auto h-full flex justify-center items-center">
+      <div className="text-center">
+        <p>
+          You must{" "}
+          <button
+            type="button"
+            className="font-medium text-blue-500 hover:underline"
+            onClick={() => {
+              signIn("google")
+            }}
+          >
+            login
+          </button>{" "}
+          to use this service.
+        </p>
+      </div>
+    </main>
+  )
 }
